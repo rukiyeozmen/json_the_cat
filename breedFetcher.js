0f67981d74ca30e0e431263d76ba7cb1d760ca79
Rukiye Ozmen <rukiiye42@gmail.com>
2023-04-14 00:52:34 +0000

    added unity test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,21 @@
 const request = require("request");
-const breedName = process.argv.slice(2);
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, function(error, response, body) {
+    const data = JSON.parse(body);
+    const breed = data[0];
+    if (error) {
+      console.log("ERROR: ", error);
+      return;
+    }
+    if (breed) {
+      console.log(breed.description);
+    } else {
+      console.log("Breed not found");
+    }
 
-request(url, function(error, response, body) {
-  const data = JSON.parse(body);
-  const breed = data[0];
-  if (error) {
-    console.log("ERROR: ", error);
-    return;
-  }
-  if (breed) {
-    console.log(breed.description);
-  } else {
-    console.log("breed is not found");
-  }
+  });
+};
 
-});
-
+module.exports = { fetchBreedDescription };
